505fb70ca08510a7f0b5b32fc30ef02941b84fde
zhangyifei <zhangyifei@wutongchain.com>
2020-05-13 20:12:50 +0800

    新建交易

--- src/main/java/com/tj/domain/StoreTx.java
+++ src/main/java/com/tj/domain/StoreTx.java
@@ -2,8 +2,6 @@ package com.tj.domain;
 
 import lombok.Data;
 
-import static com.tj.util.UtilTool.getRandomByte;
-
 @Data
 public class StoreTx {
 
@@ -12,21 +10,6 @@ public class StoreTx {
     // 隐私存证安全
     private SecretPair[] secretPair;
 
-    @Override
-    public String toString() {
-        return "{" +
-                "\"data\":\"123\"" +
-                ", \"secretPair\":\"\"" +
-                "}";
-    }
-
-    public static void main(String[] args) {
-        StoreTx storeTx = new StoreTx();
-        byte[] data = getRandomByte(16);
-        storeTx.setData(data);
-
-        System.out.println(storeTx.toString());
-    }
 }
 
 
